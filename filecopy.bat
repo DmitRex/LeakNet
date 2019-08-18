4ea8362d514f12a858e1b57d54ae49b29bf5b910
UAVXP <uavxp29@gmail.com>
2019-08-18 23:52:21 +0300

    filecopy.bat adjustments

--- filecopy.bat
+++ filecopy.bat
@@ -2,3 +2,6 @@
 @if exist %2 attrib -r %2
 @if exist %2 del %2
 @copy %1 %2
+:: @if exist %2 attrib +r %2
+
+:: VXP: Well, I've probably messed it up with filecopy_leavewriteable.bat, but I'm not going to change every filecopy.bat calls in all the projects since this is working just fine for now.
