799bbda5fa44aefd6897bf113838f38780e10c9f
UAVXP <uavxp29@gmail.com>
2019-08-27 01:45:25 +0300

    Fixes #211 - now the game shouldn't crash immediately after map loading Precision-wise

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -5741,7 +5741,11 @@ void CBasePlayer::SetupVPhysicsShadow( CPhysCollide *pStandModel, const char *pS
 	solid.params = g_PhysDefaultObjectParams;
 	solid.params.mass = 85.0f;
 //	solid.params.inertia = 1e24f;
-	solid.params.inertia = 0;
+//	solid.params.inertia = 0;
+
+	// VXP: 1e17f is a number that makes it works, but I leave 1e16f instead so it has a little overhead I guess
+	// But why? It shouldn't change very much anyway.
+	solid.params.inertia = 1e16f;
 	solid.params.enableCollisions = false;
 	//disable drag
 	solid.params.dragCoefficient = 0;
