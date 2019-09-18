c7d42f12b70d2205f399131efb2803a840872539
UAVXP <uavxp29@gmail.com>
2019-09-18 15:59:47 +0300

    printdummy stuff

--- utils/printdummy1/printdummy1.cpp
+++ utils/printdummy1/printdummy1.cpp
@@ -43,9 +43,13 @@ int main( int argc, char **argv )
 		return -1;
 	}
 
-	mstudiodummy1_t *dummy1 = pStudioHdr->pDummy1( 0 );
-	if ( dummy1->dummy1 != 0 )
-		printf( "%s has %d\r\n", pStudioHdr->name, dummy1->dummy1 );
+	//mstudiodummy1_t *dummy1 = pStudioHdr->pDummy1( 0 );
+	//if ( dummy1->dummy1 != 0 )
+	//	printf( "%s has %d\r\n", pStudioHdr->name, dummy1->dummy1 );
+
+	mstudiodummy2_t *dummy2 = pStudioHdr->pDummy2( 0 );
+	if ( dummy2->dummy1 != -1 )
+		printf( "%s has %d\r\n", pStudioHdr->name, dummy2->dummy1 );
 
 	fclose( fp );
 	return 0;
